556ab29ca4c926dc9b0d452e724fd682158fd6c3
yhuan <cokekokotea@gmail.com>
2018-12-27 01:18:44 +0800

    添加URL,文章分类和文章信息所需常量

---
+++ common/const.js
@@ -0,0 +1,16 @@
+// url 中的 path 相关常量
+export const URL_PATH_CATEGORY_TEXT = 'category';
+export const URL_PATH_ARTICLE_TEXT = 'p';
+export const URL_PATH_ABOUT_TEXT = 'about';
+export const URL_PATH_ALL_CATEGORY_NAME = 'all';
+
+// 文章分类定义的相关常量
+export const CATEGORY_DATA_NAME_TEXT = 'category_name';
+export const CATEGORY_DATA_ARTICLE_LIST_TEXT = 'article_list';
+export const CATEGORY_DATA_ALL_NAME = '全部';
+
+// 文章数据定义的相关常量
+export const ARTICLE_DATA_ID_TEXT = 'id';
+export const ARTICLE_DATA_TIME_TEXT = 'time';
+export const ARTICLE_DATA_TITLE_TEXT = 'title';
+export const ARTICLE_DATA_CONTENT_TEXT = 'content';

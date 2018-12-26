aa28d2c8d149718762f39d2f4409da01c7334974
yhuan <cokekokotea@gmail.com>
2018-12-27 01:23:20 +0800

    添加了测试数据

--- next.config.js
+++ next.config.js
@@ -2,11 +2,23 @@ module.exports = {
   exportPathMap: () => {
     return {
       '/': { page: '/' },
-      '/category/Java': { page: '/' , query: { category: 'Java' }},
-      '/category/Learn': { page: '/' , query: { category: 'Learn' }},
-      '/p/learn-java': { page: '/', query: { title: 'Learn Java' }},
-      '/p/learn-spring': { page: '/', query: { title: 'Learn Spring' } },
-      '/p/learn-english': { page: '/', query: { title: 'Learn English' } },
+      '/about': { page: '/about' },
+
+      '/category/X9I_B8oQD': { page: '/'},
+      '/category/lBGgO09j9X': { page: '/'},
+      '/category/0YcMyANhFQ': { page: '/'},
+      '/category/k0DarKg9c': { page: '/'},
+      '/category/all': { page: '/'},
+
+      '/category/X9I_B8oQD/p/ibEJiP-qv': { page: '/'},
+      '/category/X9I_B8oQD/p/VZIuEcM6Vu': { page: '/'},
+      '/category/lBGgO09j9X/p/2JFahoWnmu': { page: '/'},
+      '/category/0YcMyANhFQ/p/HSwYOKmTU2': { page: '/'},
+
+      '/category/all/p/ibEJiP-qv': { page: '/'},
+      '/category/all/p/VZIuEcM6Vu': { page: '/'},
+      '/category/all/p/2JFahoWnmu': { page: '/'},
+      '/category/all/p/HSwYOKmTU2': { page: '/'},
     }
   }
 };

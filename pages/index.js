7da5a3b56adf6669d59887d87188ae540b6f41b7
yhuan <cokekokotea@gmail.com>
2018-12-27 01:21:49 +0800

    添加了注释和获取数据以后传递给对应组件

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,68 @@
-import {fetch} from '../helper';
+import Head from 'next/head';
 
 import Container from '../components/Container';
 import Category from '../components/Category';
 import ArticleList from '../components/ArticleList';
-import Content from '../components/Content';
+import Article from '../components/Article';
+import {fetch, url} from '../helper'
+import * as COMMON_CONST from '../common/const';
 
 const Index = (props) => (
   <Container>
-    <Category categoryList={props.categoryList} />
-    <ArticleList articleList={props.articleList} />
-    <Content articleContent={props.articleContent} />
+    <Head>
+      <title>rbackrock`s blog</title>
+    </Head>
+    <Category categoryList={props.categoryList} pathParams={props.pathParams} />
+    <ArticleList articleList={props.articleList} pathParams={props.pathParams} />
+    <Article article={props.article} pathParams={props.pathParams} />
   </Container>
 );
 
-Index.getInitialProps = ({pathname, query, asPath}) => {
-  console.log(pathname);
-  console.log(query);
-  console.log(asPath);
-  console.log('---');
+/**
+ *
+ * 博客只使用了 index 和 about 两个页面，页面中文章所有数据依赖 URL 地址信息来获取
+ * 博客整体内容区域分为3个部分
+ *
+ * 分类列表：不管点分类还是具体文章，分类列表数据都是一致的
+ * 文章列表：数据依赖于 URL 中关于 category 的值
+ * 文章内容：一句依赖于 URL 中关于 p 的值
+ *
+ * 博客 URL 有如下类型：
+ * http://ip:port 首页
+ * http://ip:port/category/[分类id] 当点击分类列表某一分类链接时
+ * http://ip:port/category/[分类id]/p/[文章id] 当点击文章列表某一具体文章链接时
+ * http://ip:port/about 当点击"关于"链接时
+ *
+ * 特殊的 URL 类型：
+ * http://ip:port/category/all 当点击分类列表中"全部"分类链接时
+ * http://ip:port/category/all/p/[文章id] 在"全部"分类的情况下，点击文章列表某一具体文章链接时
+ *
+ */
+Index.getInitialProps = ({asPath}) => {
+  const categoryId = url.findPathParameterValue(asPath, COMMON_CONST.URL_PATH_CATEGORY_TEXT);
+  const articleId = url.findPathParameterValue(asPath, COMMON_CONST.URL_PATH_ARTICLE_TEXT);
+
+  const categoryList = fetch.findAllCategory();
+  const articleList = fetch.findArticleListByCategory(categoryId);
+  let article = null;
+
+  if (articleId) {
+    article = fetch.findArticleById(articleId);
+  } else {
+    if (articleList.length > 0) {
+      article = fetch.findArticleById(articleList[0]['id']);
+    }
+  }
 
   return {
-    categoryList: fetch.findAllCategory(),
-    articleList: fetch.findArticleListByCategory(query['category']),
-    articleContent: fetch.findArticleByTitle(query['title'])
-  };
+    pathParams: {
+      [COMMON_CONST.URL_PATH_CATEGORY_TEXT]: categoryId,
+      [COMMON_CONST.URL_PATH_ARTICLE_TEXT]: articleId,
+    },
+    categoryList,
+    articleList,
+    article
+  }
 };
 
 export default Index;

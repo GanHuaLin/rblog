dc45ec13ce55e0d392472449d71ad2fa5ee60fc5
yhuan <cokekokotea@gmail.com>
2018-12-28 23:03:11 +0800

    修改了了保存 markdown 文章的 post 目录为 _post

--- src/generator/helper.js
+++ src/generator/helper.js
@@ -3,21 +3,21 @@ const _ = require('lodash');
 const moment = require('moment');
 const shortId = require('shortid');
 
-const postPath = `${process.cwd()}/post`;
+const postPath = `${process.cwd()}/_post`;
 
 /**
  * markdown 文件规则：
  * 1. 任何 markdown 必须隶属一个分类文件夹
- * 2. 分类文件夹必须放在根目录的 post 文件夹下
+ * 2. 分类文件夹必须放在根目录的 _post 文件夹下
  * 3. markdown 文件名必须为 [文章标题]-[时间].md 的形式，例如：[Learn JavaScript]-[20181225].md
  *
  * /post 文件夹规则：
- * 1. /post 文件夹下只能有文件夹，不能有任何文件，且该该文件夹表示文章分类
- * 2. /post 文件夹下的分类文件夹里不能再有文件夹
- * 3. /post 文件夹下的分类文件夹里只能放对应分类的文章
- * 4. /post 文件夹下的分类文件夹里不能放除 markdown 以外的任何文件，且 markdown 文件必须要有 .md 后缀名
+ * 1. /_post 文件夹下只能有文件夹，不能有任何文件，且该该文件夹表示文章分类
+ * 2. /_post 文件夹下的分类文件夹里不能再有文件夹
+ * 3. /_post 文件夹下的分类文件夹里只能放对应分类的文章
+ * 4. /_post 文件夹下的分类文件夹里不能放除 markdown 以外的任何文件，且 markdown 文件必须要有 .md 后缀名
  *
- * 该方法用于提取 /post 文件夹下的文章，然后根据信息构建特殊结构的文章元数据对象返回
+ * 该方法用于提取 /_post 文件夹下的文章，然后根据信息构建特殊结构的文章元数据对象返回
  *
  * 如下返回值有特殊含义：
  * 1. 空对象 {} 表示有分类文件夹但是该分类下没有文章
@@ -28,7 +28,7 @@ const postPath = `${process.cwd()}/post`;
  * @param maxDirLevel 最大文件夹层级
  * @param currDirName 当前文件夹名称
  * @param result 当前分类下文章信息构建体对象
- * @returns {null} 扫描 /post 文件夹，构建一个有分类和该分类下所有文章信息的对象并且返回，例如:
+ * @returns {null} 扫描 /_post 文件夹，构建一个有分类和该分类下所有文章信息的对象并且返回，例如:
  * {'3LTPYxNCLR': {ategory_name: 'Program', article_list: [{"id": "VZIuEcM6Vu", "time": "20181225", "title": "Learn Spring"}]}}
  */
 
@@ -48,7 +48,7 @@ function fetchArticleMeta({filePath=postPath, countLevel=1, maxDirLevel=2, currD
         if (countLevel < maxDirLevel) {
           fetchArticleMeta({filePath:currFilePath, countLevel: countLevel + 1, currDirName: currFileName, result});
         } else {
-          throw 'post 文件夹下有目录超过两层';
+          throw '_post 文件夹下有目录超过两层';
         }
       } else if (fileStat.isFile()) {
         const markdownFileNameFormatRegExp = /\[(.{1,})\]-\[(\d{8})\]\.md/g; // 递归代码体中引入比如外部模块会有问题
@@ -64,7 +64,7 @@ function fetchArticleMeta({filePath=postPath, countLevel=1, maxDirLevel=2, currD
 
         if (countLevel === 1) {
           if (currFileName !== '.gitignore') {
-            throw 'post 文件夹下只能有分类文件夹或者.gitignore文件，不能有除此以外的任何文件';
+            throw '_post 文件夹下只能有分类文件夹或者.gitignore文件，不能有除此以外的任何文件';
           }
         } else if (!markdownFileNameFormatRegExp.test(currFileName)) {
           throw 'markdown 文件名称不符合规则';

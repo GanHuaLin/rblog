0519a3dd0a8f2fe923e1a39a195e16722cf4bd8f
yhuan <cokekokotea@gmail.com>
2018-12-28 16:30:16 +0800

    写入硬盘的文章元数据和文章列表文件相互依赖，必须同时存在，对外暴露 run 方法，且要么同时成功，要么同时失败

--- src/generator/db.js
+++ src/generator/db.js
@@ -1,10 +1,11 @@
 const fs = require('fs');
+const helper = require('./helper');
 
 const dbPath = `${process.cwd()}/db`;
 const ARTICLE_META_FILE_NAME = 'article-meta.json';
 const ARTICLE_LIST_FILE_NAME = 'article-list.json';
 
-exports.generateArticleMetaDb = (articleMeta) => {
+function generateArticleMetaDb(articleMeta) {
   const writeArticleMetaDbStream = fs.createWriteStream(`${dbPath}/${ARTICLE_META_FILE_NAME}`);
   writeArticleMetaDbStream.end(JSON.stringify(articleMeta));
 
@@ -12,9 +13,9 @@ exports.generateArticleMetaDb = (articleMeta) => {
     writeArticleMetaDbStream.on('finish', resolve);
     writeArticleMetaDbStream.on('error', reject);
   });
-};
+}
 
-exports.generateArticleListDb = (articleList) => {
+function generateArticleListDb (articleList) {
   const writeArticleListDbStream = fs.createWriteStream(`${dbPath}/${ARTICLE_LIST_FILE_NAME}`);
   writeArticleListDbStream.end(JSON.stringify(articleList));
 
@@ -22,4 +23,12 @@ exports.generateArticleListDb = (articleList) => {
     writeArticleListDbStream.on('error', reject);
     writeArticleListDbStream.on('finish', resolve);
   });
+}
+
+exports.run = () => {
+  const articleMetaAndListCombine = helper.fetchArticleMetaAndList();
+  return Promise.all([
+    generateArticleMetaDb(articleMetaAndListCombine.articleMeta),
+    generateArticleListDb(articleMetaAndListCombine.articleList)
+  ])
 };

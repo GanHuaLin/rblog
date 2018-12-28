b63cc3429a79b90da084822fd7786cb600cfa59c
yhuan <cokekokotea@gmail.com>
2018-12-28 14:54:13 +0800

    拆分构建文章元数据和文章列表两个文件方法，并且用 Promise 包裹

--- generator/db.js
+++ generator/db.js
@@ -1,40 +1,25 @@
 const fs = require('fs');
-const helper = require('./helper');
 
 const dbPath = `${process.cwd()}/db`;
 const ARTICLE_META_FILE_NAME = 'article-meta.json';
 const ARTICLE_LIST_FILE_NAME = 'article-list.json';
 
-console.log('请稍等...');
-
-try {
-  const articleMeta = helper.fetchArticleMeta({});
-  helper.clearArticleMetaData(articleMeta);
-  const articleList = helper.fetchArticleList(articleMeta);
-
+exports.generateArticleMetaDb = (articleMeta) => {
   const writeArticleMetaDbStream = fs.createWriteStream(`${dbPath}/${ARTICLE_META_FILE_NAME}`);
-  const writeArticleListDbStream = fs.createWriteStream(`${dbPath}/${ARTICLE_LIST_FILE_NAME}`);
-
   writeArticleMetaDbStream.end(JSON.stringify(articleMeta));
+
+  return new Promise((resolve, reject) => {
+    writeArticleMetaDbStream.on('finish', resolve);
+    writeArticleMetaDbStream.on('error', reject);
+  });
+};
+
+exports.generateArticleListDb = (articleList) => {
+  const writeArticleListDbStream = fs.createWriteStream(`${dbPath}/${ARTICLE_LIST_FILE_NAME}`);
   writeArticleListDbStream.end(JSON.stringify(articleList));
 
-  writeArticleMetaDbStream.on('error', (error) => {
-    helper.removeDirectoryFile(dbPath);
-    console.log(`sorry error: ${error}`);
+  return new Promise((resolve, reject) => {
+    writeArticleListDbStream.on('error', reject);
+    writeArticleListDbStream.on('finish', resolve);
   });
-
-  writeArticleListDbStream.on('error', (error) => {
-    helper.removeDirectoryFile(dbPath);
-    console.log(`sorry error: ${error}`);
-  });
-
-  writeArticleMetaDbStream.on('finish', () => {
-    console.log('article-meta.json 生成成功');
-  });
-
-  writeArticleListDbStream.on('finish', () => {
-    console.log('article-list.json 生成成功');
-  });
-} catch (e) {
-  console.log(e);
-}
+};

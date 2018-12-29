2e25ecd9b6025a8f4b0d6692891207c04540d38a
yhuan <cokekokotea@gmail.com>
2018-12-29 17:17:16 +0800

    修正了进行 stream 输入流的正确的 Promise 使用方法

--- src/generate.js
+++ src/generate.js
@@ -7,10 +7,11 @@ const ARTICLE_META_FILE_NAME = 'article-meta.json';
 const ARTICLE_LIST_FILE_NAME = 'article-list.json';
 
 function generateArticleMetaDb(articleMeta) {
-  const writeArticleMetaDbStream = fs.createWriteStream(`${dbPath}/${ARTICLE_META_FILE_NAME}`);
-  writeArticleMetaDbStream.write(JSON.stringify(articleMeta));
-
   return new Promise((resolve, reject) => {
+    const writeArticleMetaDbStream = fs.createWriteStream(`${dbPath}/${ARTICLE_META_FILE_NAME}`);
+    writeArticleMetaDbStream.write(JSON.stringify(articleMeta));
+
+    writeArticleMetaDbStream.end();
     writeArticleMetaDbStream.on('finish', resolve);
     writeArticleMetaDbStream.on('error', () => {
       util.removeDirectoryFile(dbPath, ['.gitignore']);
@@ -20,10 +21,11 @@ function generateArticleMetaDb(articleMeta) {
 }
 
 function generateArticleListDb (articleList) {
-  const writeArticleListDbStream = fs.createWriteStream(`${dbPath}/${ARTICLE_LIST_FILE_NAME}`);
-  writeArticleListDbStream.write(JSON.stringify(articleList));
-
   return new Promise((resolve, reject) => {
+    const writeArticleListDbStream = fs.createWriteStream(`${dbPath}/${ARTICLE_LIST_FILE_NAME}`);
+    writeArticleListDbStream.write(JSON.stringify(articleList));
+
+    writeArticleListDbStream.end();
     writeArticleListDbStream.on('finish', resolve);
     writeArticleListDbStream.on('error', () => {
       util.removeDirectoryFile(dbPath, ['.gitignore']);

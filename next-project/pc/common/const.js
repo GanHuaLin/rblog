67ecf0c3537b07377902598a0f5bb4033dec119c
yhuan <cokekokotea@gmail.com>
2019-01-03 04:01:09 +0800

    添加分类文章总数常量

--- next-project/pc/common/const.js
+++ next-project/pc/common/const.js
@@ -8,10 +8,11 @@ export const URL_PATH_ALL_CATEGORY_NAME = 'all';
 export const CATEGORY_DATA_NAME_TEXT = 'category_name';
 export const CATEGORY_DATA_ID_TEXT = 'category_id';
 export const CATEGORY_DATA_ARTICLE_LIST_TEXT = 'article_list';
+export const CATEGORY_DATA_ARTICLE_NUM_TEXT = 'category_num';
 export const CATEGORY_DATA_ALL_NAME = '全部';
 
 // 文章数据定义的相关常量
 export const ARTICLE_DATA_ID_TEXT = 'id';
-export const ARTICLE_DATA_TIME_TEXT = 'time';
+export const ARTICLE_DATA_DATE_TEXT = 'date';
 export const ARTICLE_DATA_TITLE_TEXT = 'title';
 export const ARTICLE_DATA_CONTENT_TEXT = 'content';

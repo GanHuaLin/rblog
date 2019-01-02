0e421663b14023b2ededbc855a03111dc1d7ca04
yhuan <cokekokotea@gmail.com>
2019-01-03 04:51:38 +0800

    修改代码块展示，去除边框，添加盒子阴影

--- next-project/pc/components/markdown/CodeBlock.js
+++ next-project/pc/components/markdown/CodeBlock.js
@@ -34,31 +34,44 @@ class CodeBlock extends Component {
   render() {
     return (
       <div className='container' ref={this.wrapperRef}>
-        <SyntaxHighlighter
-          className='syntax-high-lighter-container'
-          wrapLines={true}
-          language={this.props.language}
-          style={dark}
-          customStyle={{
-            margin: '0',
-            overflow: 'hidden',
-            position: 'relative',
-            flex: 1,
-          }}
-          codeTagProps={{
-            style: {
-              display: 'inline-block',
-              padding: '0 1.6em 0 0',
-            }
-          }}
-        >{this.props.value}
-        </SyntaxHighlighter>
+        <div className='wrapper'>
+          <SyntaxHighlighter
+            className='syntax-high-lighter-container'
+            wrapLines={true}
+            language={this.props.language}
+            style={dark}
+            customStyle={{
+              margin: '0',
+              overflow: 'hidden',
+              position: 'relative',
+              flex: 1,
+              border: 'none',
+              boxShadow: '0 3px 8px rgba(69,18,10,.4)',
+            }}
+            codeTagProps={{
+              style: {
+                display: 'inline-block',
+                padding: '0 1.6em 0 0',
+              }
+            }}
+          >{this.props.value}
+          </SyntaxHighlighter>
+        </div>
 
         <style jsx>{`
           .container {
             display: flex;
             position: relative;
             align-items:center;
+          }
+
+          .wrapper {
+            display: flex;
+            width: 100%;
+            position: relative;
+            align-items: center;
+            border-radius: 0.5em;
+            box-shadow: 0 0 2px rgba(126,50,0,.1) inset, 0 1px 4px rgba(58,15,0,.2);
           }
         `}</style>
       </div>

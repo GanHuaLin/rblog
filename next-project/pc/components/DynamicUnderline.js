4ffb7af5669e0fd8d4c3acf2f5287f08cb008418
yhuan <cokekokotea@gmail.com>
2019-01-04 20:20:18 +0800

    拆分闪烁下划线组件

---
+++ next-project/pc/components/DynamicUnderline.js
@@ -0,0 +1,38 @@
+import { Fragment } from 'react';
+
+export default () => (
+  <Fragment>
+    {/*<span className='motto'>&nbsp;_</span>*/}
+    &nbsp;<span className='motto'></span>
+
+    <style jsx>{`
+      .motto {
+        display: inline-block;
+        height: 1px;
+        width: 15px;
+        border-bottom: 2px solid;
+        opacity: 1;
+
+        -webkit-animation: blink 0.9s infinite;
+        -moz-animation: blink 0.9s infinite;
+        animation: blink 0.9s infinite;
+      }
+
+      @keyframes blink {
+        0% { opacity:1; }
+        50% { opacity:0; }
+        100% { opacity:1; }
+      }
+      @-webkit-keyframes blink {
+        0% { opacity:1; }
+        50% { opacity:0; }
+        100% { opacity:1; }
+      }
+      @-moz-keyframes blink {
+        0% { opacity:1; }
+        50% { opacity:0; }
+        100% { opacity:1; }
+      }
+    `}</style>
+  </Fragment>
+);

92ac03093c527c38fbd76e9fae69d67af8f6a4f9
yhuan <cokekokotea@gmail.com>
2019-01-04 15:36:04 +0800

    删除废弃代码

--- next-project/pc/components/Layout.js
+++ next-project/pc/components/Layout.js
@@ -8,7 +8,6 @@ export default (props) => (
       .wrapper {
         width: 100%;
         height: 100%;
-        // min-height: 700px;
         min-width: 1280px;
         overflow: hidden;
       }

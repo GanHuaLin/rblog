9599ba924d15b93c8251fe602f9b4fa0101aaf6c
yhuan <cokekokotea@gmail.com>
2019-01-04 20:19:32 +0800

    添加关于页面

--- next-project/pc/pages/about.js
+++ next-project/pc/pages/about.js
@@ -1,11 +1,38 @@
+import Layout from '../components/Layout';
+import PagerContainer from '../components/PaperContainer';
+import DynamicUnderline from '../components/DynamicUnderline';
+
 export default () => (
-  <div>
-    About
+  <Layout>
+    <div className="container">
+      <PagerContainer>
+        <p>大家好，我叫 rbackrock 网名也取过回滚滚、逆十方</p><br/>
+        <p>喜欢计算机，愿望之一是可以撸代码撸到老</p><br/>
+        <p>有人说，优秀的程序员，同时也是一名优秀的作家。希望以这亲手打造的博客为契机，开始记录技术和生活的点滴</p><br/>
+        <p>如果你喜欢本博客，欢迎使用和 Fork 项目，源代码<a className='link' target="_black" href="https://github.com/rbackrock/rblog">点这里</a></p><br/>
+        <p>电邮：cokekokotea#gmail.com</p><br/>
+        <p>感谢你的访问<DynamicUnderline /></p><br/>
+      </PagerContainer>
+    </div>
 
     <style jsx>{`
-      div {
+      .container {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        position: relative;
+        width: 100%;
         height: 95%;
       }
+
+      .link {
+        color: #b4a08e;
+        text-decoration: none;
+      }
+
+      .link:hover {
+        color: #645647;
+      }
     `}</style>
-  </div>
+  </Layout>
 );

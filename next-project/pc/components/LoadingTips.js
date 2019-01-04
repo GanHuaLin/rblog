73068275326ec14773cab84c7f84edba8017f809
yhuan <cokekokotea@gmail.com>
2019-01-05 01:27:49 +0800

    添加载入中提示组件

---
+++ next-project/pc/components/LoadingTips.js
@@ -0,0 +1,41 @@
+export default () => (
+  <div className='wrapper'>
+    <span className='text'>Loading...</span>
+
+    <style jsx>{`
+      .wrapper {
+        position: absolute;
+        display: table;
+        height: 100%;
+        z-index: 999;
+        width: 100%;
+      }
+
+      .text {
+        display: table-cell;
+        vertical-align: middle;
+        text-align: center;
+        font-size: 36px;
+        color: #635753;
+
+        animation: blink 0.9s infinite;
+      }
+
+      @keyframes blink {
+        0% { opacity:1; }
+        50% { opacity:0; }
+        100% { opacity:1; }
+      }
+      @-webkit-keyframes blink {
+        0% { opacity:1; }
+        50% { opacity:0; }
+        100% { opacity:1; }
+      }
+      @-moz-keyframes blink {
+        0% { opacity:1; }
+        50% { opacity:0; }
+        100% { opacity:1; }
+      }
+    `}</style>
+  </div>
+);

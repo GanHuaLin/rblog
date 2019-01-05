61a71714ef45c5f0163076735e3d3bbe56389304
yhuan <cokekokotea@gmail.com>
2019-01-05 20:30:30 +0800

    修改类名

--- next-project/pc/pages/_app.js
+++ next-project/pc/pages/_app.js
@@ -2,7 +2,7 @@ import React from 'react'
 import App, { Container } from 'next/app'
 import Head from 'next/head'
 
-export default class MyApp extends App {
+export default class BlogApp extends App {
   static async getInitialProps({ Component, router, ctx }) {
     let pageProps = {};
 

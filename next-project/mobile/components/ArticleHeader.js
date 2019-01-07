c3c2cd59635ce6681be9a70e79d44b98f1f16bc5
yhuan <cokekokotea@gmail.com>
2019-01-08 00:58:06 +0800

    完成文章页面标题数据和显示

--- next-project/mobile/components/ArticleHeader.js
+++ next-project/mobile/components/ArticleHeader.js
@@ -1,3 +1,70 @@
-export default () => (
-  <h1>title</h1>
-);
+import React, { Component } from 'react';
+import { withRouter } from 'next/router'
+import HeaderLayout from './HeaderLayout';
+
+class ArticleHeader extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  btnBackHandle = () => {
+    this.props.router.back();
+  };
+
+  btnAboutHandle = () => {
+    this.props.router.push('/about');
+  };
+
+  render() {
+    return (
+      <HeaderLayout>
+        <div className="container">
+          <div className="left">
+            <div onClick={this.btnBackHandle} className="btn"><span className="text">返回</span></div>
+          </div>
+          <div className="center">
+            <div className="title">rbackrock`s 的博客</div>
+          </div>
+          <div className="right">
+            <div onClick={this.btnAboutHandle} className="btn"><span className="text">关于</span></div>
+          </div>
+        </div>
+
+        <style jsx>{`
+          .container {
+            display: flex;
+            height: 100%;
+            box-sizing: border-box;
+            color: #fff;
+
+            .left, .center, .right {
+              height: 100%;
+              line-height: 6vh;
+              text-align: center;
+            }
+
+            .left {
+              flex: 1;
+            }
+
+            .center {
+              flex: 6;
+              text-align: center;
+
+              .title {
+                font-size: 4vw;
+                font-weight: 600;
+              }
+            }
+
+            .right {
+              flex: 1;
+            }
+          }
+        `}</style>
+      </HeaderLayout>
+    );
+  }
+}
+
+export default withRouter(ArticleHeader);

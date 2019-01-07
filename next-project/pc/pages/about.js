3308619dc5aaff306a4b0355a4efc0a3a5a4339a
yhuan <cokekokotea@gmail.com>
2019-01-08 00:59:50 +0800

    修改关于我描述信息

--- next-project/pc/pages/about.js
+++ next-project/pc/pages/about.js
@@ -6,7 +6,7 @@ export default () => (
   <Layout>
     <div className="container">
       <PagerContainer>
-        <p>大家好，我叫 rbackrock 网名也取过回滚滚、逆十方</p><br/>
+        <p>大家好，我叫 rbackrock 网名也取过回滚滚</p><br/>
         <p>喜欢计算机，愿望之一是可以撸代码撸到老</p><br/>
         <p>有人说，优秀的程序员，同时也是一名优秀的作家。希望以这亲手打造的博客为契机，开始记录技术和生活的点滴</p><br/>
         <p>如果你喜欢本博客，欢迎使用和 Fork 项目，源代码<a className='link' target="_black" href="https://github.com/rbackrock/rblog">点这里</a></p><br/>

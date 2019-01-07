0c2cdb8491f608320d0fc7e768fbb2e95ff86fb8
yhuan <cokekokotea@gmail.com>
2019-01-08 00:58:51 +0800

    完成文本内容组件基本构架

--- next-project/mobile/components/ArticleContent.js
+++ next-project/mobile/components/ArticleContent.js
@@ -1,15 +1,63 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from "redux";
+import * as actionCreator from '../common/store/actionCreator';
+import BScroll from 'better-scroll';
+import {withRouter} from "next/dist/lib/router";
+import ContentLayout from './ContentLayout';
+import ReactMarkdown from 'react-markdown';
+import * as COMMON_CONST from "../../common/const";
+import * as url from "../../common/url";
 
 class ArticleContent extends Component {
   constructor(props) {
     super(props);
   }
 
+  componentDidMount() {
+    const articleId = url.findPathParameterValue(this.props.router.asPath, COMMON_CONST.URL_PATH_ARTICLE_TEXT);
+    this.props.actions.fetchArticle(articleId);
+  }
+
   render() {
     return (
-      <div>123</div>
+      <ContentLayout>
+        <div className="container">
+          {
+            this.props.article ? (
+              <div className="content">
+                <h1>文章标题</h1>
+                <ReactMarkdown
+                  source={this.props.article[COMMON_CONST.ARTICLE_DATA_CONTENT_TEXT]}
+                  escapeHtml={true}
+                  linkTarget='_blank'
+                  renderers={{
+                    // code: CodeBlock,
+                  }}
+                />
+              </div>
+            ) : null
+          }
+        </div>
+
+        <style>{`
+
+        `}</style>
+      </ContentLayout>
     );
   };
 }
 
-export default ArticleContent;
+const mapStateToProps = (state) => {
+  return {
+    article: state.getIn(['article'])
+  }
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    actions: bindActionCreators({...actionCreator}, dispatch)
+  }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(ArticleContent));

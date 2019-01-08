27375d73469da20a29000502d92e64935bc317f3
yhuan <cokekokotea@gmail.com>
2019-01-08 18:01:36 +0800

    添加移动端关于页面

--- next-project/mobile/pages/about.js
+++ next-project/mobile/pages/about.js
@@ -1,3 +1,21 @@
-export default () => (
-  <h1>About</h1>
-);
+import React, { Component } from 'react';
+import Layout from '../components/Layout';
+import AboutHeader from '../components/AboutHeader';
+import AboutContent from '../components/AboutContent';
+
+class About extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <Layout>
+        <AboutHeader />
+        <AboutContent />
+      </Layout>
+    );
+  }
+}
+
+export default About;

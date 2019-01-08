62f9f97f3b2a01b335738fcf2e5dc1ae92b11dba
yhuan <cokekokotea@gmail.com>
2019-01-08 18:02:46 +0800

    添加移动端动态下划线组件

---
+++ next-project/mobile/components/DynamicUnderline.js
@@ -0,0 +1,35 @@
+import { Fragment } from 'react';
+
+export default () => (
+  <Fragment>
+    {/*<span className='motto'>&nbsp;_</span>*/}
+    &nbsp;<span className='motto'></span>
+
+    <style jsx>{`
+      .motto {
+        display: inline-block;
+        height: 1vh;
+        width: 3vw;
+        border-bottom: 0.4vw solid;
+        opacity: 1;
+        animation: blink 0.9s infinite;
+      }
+
+      @keyframes blink {
+        0% { opacity:1; }
+        50% { opacity:0; }
+        100% { opacity:1; }
+      }
+      @-webkit-keyframes blink {
+        0% { opacity:1; }
+        50% { opacity:0; }
+        100% { opacity:1; }
+      }
+      @-moz-keyframes blink {
+        0% { opacity:1; }
+        50% { opacity:0; }
+        100% { opacity:1; }
+      }
+    `}</style>
+  </Fragment>
+);

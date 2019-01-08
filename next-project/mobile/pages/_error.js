c04f0a9d3bb25921de363cf2db855a858840cd0f
yhuan <cokekokotea@gmail.com>
2019-01-08 18:01:27 +0800

    添加移动端错误页面

--- next-project/mobile/pages/_error.js
+++ next-project/mobile/pages/_error.js
@@ -1,5 +1,8 @@
 import React from 'react'
-import { withRouter } from 'next/router'
+import { withRouter } from 'next/router';
+import Layout from '../components/Layout';
+import HeaderLayout from '../components/HeaderLayout';
+import DynamicUnderline from "../components/DynamicUnderline";
 
 class Error extends React.Component {
   static getInitialProps({ res, err }) {
@@ -13,13 +16,46 @@ class Error extends React.Component {
 
   render() {
     return (
-      <div className="container">
-        <h1>Error</h1>
+      <Layout>
+        <HeaderLayout>
+          <h1 className="err-title">错误</h1>
+        </HeaderLayout>
+
+        <div className="container">
+          <div className="wrapper">
+            <p>Balls,&nbsp;发生404错误了，回<span onClick={this.onGoBackIndexClickHandle} className='link'>首页</span>吧<DynamicUnderline /></p>
+          </div>
+        </div>
 
         <style jsx>{`
+          .err-title {
+            height: 6vh;
+            line-height: 6vh;
+            text-align: center;
+            color: #fff;
+            font-size: 4vw;
+            font-weight: 600;
+          }
 
+          .container {
+            width: 100%;
+            height: 100%;
+
+            .wrapper {
+              margin: 4vw;
+              padding: 5vw;
+              background: #fffcf6;
+              font-size: 5vw;
+              box-shadow: 0 3px 8px rgba(69,18,10,.4);
+
+              .link {
+                color: #b4a08e;
+                text-decoration: none;
+              }
+            }
+          }
         `}</style>
-      </div>
+      </Layout>
     )
   }
 }

2656990b21436024bde80d9f1d4f1c4b8576ddc3
yhuan <cokekokotea@gmail.com>
2019-01-10 15:53:07 +0800

    update

--- next-project/pc/util/url.js
+++ next-project/pc/util/url.js
@@ -1,27 +1,14 @@
-import * as COMMON_CONST from '../../common/const';
-
-// export function findPathParameterValue(path, parameter) {
-//   if (parameter) {
-//     const params = decodeURI(path).split('/');
-//     const parameterIndex = params.indexOf(parameter);
-//
-//     return parameterIndex !== -1 ? params[parameterIndex + 1] : '';
-//   }
-//
-//   return '';
-// }
-
 export function makeBlogDataUrlPath({categoryId='', articleId=''}) {
   let path = '';
 
   if (categoryId) {
-    path += `/${COMMON_CONST.URL_PATH_CATEGORY_TEXT}/${encodeURI(categoryId)}`;
+    path += `/${'category'}/${encodeURI(categoryId)}`;
   } else {
-    path += `/${COMMON_CONST.URL_PATH_CATEGORY_TEXT}/${COMMON_CONST.URL_PATH_ALL_CATEGORY_NAME}`;
+    path += `/${'category'}/${'all'}`;
   }
 
   if (articleId) {
-    path += `/${COMMON_CONST.URL_PATH_ARTICLE_TEXT}/${encodeURI(articleId)}`;
+    path += `/${'p'}/${encodeURI(articleId)}`;
   }
 
   return path;

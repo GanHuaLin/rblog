9f72c59dd908338d8c4f39aaa0c2a538e422d233
yhuan <cokekokotea@gmail.com>
2019-01-10 15:54:06 +0800

    修改移动端数据获取方式

--- next-project/mobile/pages/index.js
+++ next-project/mobile/pages/index.js
@@ -1,23 +1,55 @@
-import Layout from '../components/Layout';
-import ContentLayout from '../components/ContentLayout';
-import IndexHeader from '../components/IndexHeader';
-import ArticleList from '../components/ArticleList';
+import CategoryPage from '../components/CategoryPage';
+import ArticlePage from '../components/ArticlePage';
+import * as commonUrl from '../../common/url';
 
-const Index = () => (
-  <Layout>
-    <IndexHeader />
-    <ContentLayout>
-      <ArticleList />
-    </ContentLayout>
-  </Layout>
-);
+const server = require('../../../api/service');
+let articleMetaData = [];
+let articleListData = {};
 
-Index.getInitialProps = () => {
+try {
+  articleMetaData = require('../../../db/article-meta.json');
+  articleListData = require('../../../db/article-list.json');
+} catch (e) {
+  console.log(e);
+  throw `文章原数据格式异常，请查看 db 目录下生成的数据文件`;
+}
 
+const CATEGORY_PAGE = 1;
+const ARTICLE_PAGE = 2;
 
-  return {
+const Index = (props) => {
+  let currPage = null;
+  if (props.currPageType === CATEGORY_PAGE) {
+    currPage = <CategoryPage currentCategoryId={props.currentCategoryId} categoryList={props.categoryList} articleList={props.articleList} />
+  } else if (props.currPageType === ARTICLE_PAGE) {
+    currPage = <ArticlePage article={props.article} />
+  }
 
+  return currPage;
+};
+
+Index.getInitialProps = ({asPath}) => {
+  const fetch = server.init(articleMetaData, articleListData);
+  const categoryId = commonUrl.findPathParameterValue(asPath, 'category') || 'all';
+  const articleId = commonUrl.findPathParameterValue(asPath, 'p');
+  const returnProps = {
+    currPageType: CATEGORY_PAGE
   };
+
+
+  if (commonUrl.existPathName(asPath, 'p')) {
+    returnProps.currPageType = ARTICLE_PAGE;
+    returnProps.article = fetch.findArticleById(articleId);
+  } else if ((asPath === '/') || (commonUrl.existPathName(asPath, 'category'))) {
+    const categoryList = fetch.findAllCategory();
+    const articleList = fetch.findArticleListByCategory(categoryId);
+
+    returnProps.categoryList = categoryList;
+    returnProps.currentCategoryId = categoryId;
+    returnProps.articleList = articleList;
+  }
+
+  return returnProps;
 };
 
 export default Index;
